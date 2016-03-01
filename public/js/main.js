f7fdb7068fc0c9bdaffa23a9bf19f151b6ab9055
Alexander Teglund <alexteg@gmail.com>
2016-03-02 00:31:04 +0100

    Theremin prototype v0.0.2

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,4 @@
 (function() {
-
-  function limit(value, min, max) {
-    if(value < min) {
-      return min;
-    }
-    if(value > max) {
-      return max;
-    }
-    return value;
-  }
-
-  var frameCount = 0;
-
   var audioContext = new AudioContext();
   var gainNode = audioContext.createGain();
   var oscillator = audioContext.createOscillator();
@@ -19,31 +6,25 @@
   oscillator.connect(gainNode);
   gainNode.connect(audioContext.destination);
   gainNode.gain.value = 0;
-
   oscillator.start();
 
-  // Setup Leap loop with frame callback function
-  var controllerOptions = {enableGestures: true};
-
-  Leap.loop(controllerOptions, function(frame) {
+  Leap.loop({ enableGestures: true }, function(frame) {
     if(frame.fingers.length > 0 && typeof frame.fingers[1] != 'undefined') {
 
-      var fingerPositionX = frame.fingers[1].dipPosition[0];
-      var frequency = limit(fingerPositionX + 300, 0, 600);
+      var fingerPositionX = frame.fingers[1].dipPosition[0] + 300;
+      var frequency =Math.min(Math.max(fingerPositionX, 0), 600);
       frequency = Math.round(frequency * 2 + 30);
 
-      var fingerPositionY = frame.fingers[1].dipPosition[1];
-      fingerPositionY = limit(fingerPositionY, 0, 300);
+      var fingerPositionY = frame.fingers[1].dipPosition[1] - 100;
+      fingerPositionY = Math.min(Math.max(fingerPositionY, 0), 400);
 
-      var volume = Math.round(fingerPositionY / 3) / 100;
+      var volume = Math.round(fingerPositionY / 4) / 100;
 
-      console.log(frequency, volume);
-
+      console.log('Frequency:', frequency, 'Hz', 'Volume:', volume);
       oscillator.frequency.value = frequency;
       gainNode.gain.value = volume;
     } else {
       gainNode.gain.value = 0;
     }
-    frameCount++;
   });
 })();
